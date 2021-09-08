64daff3308e88f316ddfd87364684ae6a83b2d8f
Masiluy <71081063+kittyofheaven@users.noreply.github.com>
2021-09-08 20:20:53 +0700

    Update 17-list_comp.py

--- learn/17-list_comp.py
+++ learn/17-list_comp.py
@@ -2,5 +2,11 @@
 names = ['jennifer', 'jesselyn', 'susan', 'sophie']
 
 l = []
+print (l)
 for person in names:
-    l.append(l)
+    l.append(person)
+print (l)
+
+#klo dijadiin bahasa manusia kaya
+#print 'orang' untuk setiap orang(<- ini variable) di names (<- ini list)
+print ([orang for orang in names]) 
