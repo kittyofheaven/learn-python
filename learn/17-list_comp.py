8016ce8ed3e1c8f61a441164aa26ea59cab867d9
Masiluy <71081063+kittyofheaven@users.noreply.github.com>
2021-09-08 20:29:45 +0700

    Update 17-list_comp.py

--- learn/17-list_comp.py
+++ learn/17-list_comp.py
@@ -1,12 +1,23 @@
 #list comperhensions
 names = ['jennifer', 'jesselyn', 'susan', 'sophie']
 
+#dibawah ini adalah konten yang akan di sederhanain dengan comperhension
 l = []
-print (l)
 for person in names:
     l.append(person)
 print (l)
 
 #klo dijadiin bahasa manusia kaya
 #print 'orang' untuk setiap orang(<- ini variable) di names (<- ini list)
-print ([orang for orang in names]) 
+print ([orang for orang in names]) #ini nama comperhension ya gitulah
+
+#bisa buat operation untuk setiap elemen didalam list
+
+#dibawah ini adalah konten yang akan di sederhanain dengan comperhension
+k = []
+for ex in names:
+    k.append(ex + ' dumped me')
+print (k)
+
+#lalu dibawah ini disederhanain dengan comperhension
+print(mantan + ' dumped me' for mantan in names)
