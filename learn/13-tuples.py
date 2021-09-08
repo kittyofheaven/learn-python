ae4e880f8d278dc7fcacfa1989f5ef095fef2799
shironeko <71081063+kittyofheaven@users.noreply.github.com>
2021-09-08 17:49:02 +0700

    Update 13-tuples.py

--- learn/13-tuples.py
+++ learn/13-tuples.py
@@ -2,15 +2,15 @@ t = (1,2,3,4,5,6,7,8,9)
 #tuple seperti list tapi dengan tanda kurung, bedanya dengan list 
 #tuple tidak dapat diubah diganti dihapus dan lebih aman
 
-credit_card = (123123123121, "Hazel Handrata", '10/26', "alamat rumah b3/18", 123)
-credit_card1 = (123126723121, "Ajel Handrata", '8/21', "alamat rumah 67", 321)
+credit_card = (123123123121, "Shironeko", '10/26', "alamat rumah A3/18", 123)
+credit_card1 = (123126723121, "Shiro Neko Senpai", '8/21', "alamat rumah 67", 321)
 
 credit_cards = [credit_card, credit_card1]
 
 print(credit_cards)
 
-person_1 = ('hazel', 15, 'male')
-person_2 = ('ajel', 17, 'male')
+person_1 = ('neko', 15, 'male')
+person_2 = ('shiro', 17, 'male')
 
 
 people = [person_1, person_2]
