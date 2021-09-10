9528140578efed4c1821059b649f75cfdce5dc70
Masiluy <71081063+kittyofheaven@users.noreply.github.com>
2021-09-10 21:22:24 +0700

    Update script2.py

--- Webscraping/script2.py
+++ Webscraping/script2.py
@@ -1,3 +1,8 @@
 import pandas as pd
 import requests
 from bs4 import BeautifulSoup
+
+page = requests.get('https://www.bmkg.go.id/cuaca/prakiraan-cuaca.bmkg?Kota=Surabaya&AreaID=501306&Prov=35')
+soup = BeautifulSoup(page.content, 'html.parser')
+
+cuaca = soup.find_all(class_='prakicu-kabkota')
