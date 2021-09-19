5cf8a7b8477cab374ca1067bd662bbd9a42b41f3
shironeko <71081063+kittyofheaven@users.noreply.github.com>
2021-09-19 05:08:36 +0000

    everything

---
+++ learn/22-lambda_function.py
@@ -0,0 +1,11 @@
+# fungsi biasa 
+def jumlah(a, b):
+  c = a + b
+  return c
+
+print(jumlah(4,5))
+
+# membuat anonymous func dengan lambda
+kali = lambda a,b: a*b #auto return 
+
+print(kali(3,2))
